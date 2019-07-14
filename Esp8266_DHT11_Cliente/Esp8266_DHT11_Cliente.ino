be6e258539d7d9a0965f74b3dfcec8fef288d374
RickyMontoya11 <31260587+RickyMontoya11@users.noreply.github.com>
2019-07-14 13:41:25 -0500

    Update Esp8266_DHT11_Cliente.ino

--- Esp8266_DHT11_Cliente/Esp8266_DHT11_Cliente.ino
+++ Esp8266_DHT11_Cliente/Esp8266_DHT11_Cliente.ino
@@ -95,3 +95,103 @@ void loop()
 
   delay(60000);
 }
+
+
+
+
+
+
+#include "DHT.h"
+#include <ESP8266WiFi.h>
+#define DHTPIN 15
+#define DHTTYPE DHT22 
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+const char* ssid = "INFINITUM1961";
+const char* password = "CKe6fmtfrF";
+const char* host = "192.168.1.74";
+
+
+void setup()
+{
+  Serial.begin(9600);
+  // We start by connecting to a WiFi network
+  dht.begin();
+  pinMode(13, OUTPUT); 
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+
+    float hum = dht.readHumidity();
+    float temp = dht.readTemperature();
+    
+    Serial.print(" Humedad: ");
+    Serial.print(hum);
+    Serial.print("Temperatura: ");
+    Serial.print(temp);
+    Serial.println();
+
+ 
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  // We now create a URL for the request
+  String url = "/dht11.php";
+  String key = "?pass=LIA123456";
+  String dato1 = "&Temperatura=";
+  String dato2 = "&Humedad=";
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + key + dato1 + temp + dato2 + hum + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+
+  delay(60000);
+}
